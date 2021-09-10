1b6c44f4274b70b352ffcc3dff9349e6c2459480
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-09-10 13:35:49 +0530

    Add Java Docs

--- src/xyz/kumaraswamy/slime/lex/Lex.java
+++ src/xyz/kumaraswamy/slime/lex/Lex.java
@@ -4,11 +4,18 @@ import xyz.kumaraswamy.slime.Slime;
 
 import java.util.LinkedList;
 
+// a class to lex texts and form tokens
+// this is the first step to create a node tree
+
 public class Lex {
+
     private final String data;
     private final Slime slime;
 
     private final LinkedList<String> tokens = new LinkedList<>();
+
+    // StringBuilders to append and store
+    // the texts and numbers
 
     private final StringBuilder builder = new StringBuilder();
     private final StringBuilder number = new StringBuilder();
@@ -27,23 +34,39 @@ public class Lex {
                     ? "" : tokens.getLast();
             final int size = tokens.size() - 1;
 
+            // keep track, if we are inside the
+            // single quote or not
+
             if (ch == '\'') {
                 inside = !inside;
                 builder.append(ch);
             } else if (inside) {
+                // simply append the char
+                // if we are inside
                 builder.append(ch);
             } else if (ch == ' ' || isBlock(ch)) {
+                // a space is also used as a
+                // delimiter here
                 appendTokens();
                 if (ch != ' ') {
                     tokens.add(ch + "");
                 }
             } else if (isDigit(ch)) {
+                // check if the character is a digit
+
+                // an exception for negative and
+                // floating point numbers
+
                 if (previous.equals("-") && slime.isOperator(size == 0
                         ? "" : tokens.get(size - 1))) {
                     tokens.set(size, "-" + ch);
                 } else if (isDigits(previous) && size != -1) {
+                    // check if previous token was only digits
+                    // if so, append that with the current char
                     String append = previous + ch;
                     if (append.startsWith(".")) {
+                        // just add `0` in front of
+                        // short decimals point values like .7
                         append = '0' + append;
                     }
                     tokens.set(size, append);
@@ -51,6 +74,9 @@ public class Lex {
                     tokens.add(ch + "");
                 }
             } else {
+                // do the same thing for characters
+                // as did with the numbers
+
                 appendTokens();
                 if (pos > 0 && data.charAt(pos - 1) == ' ') {
                     tokens.add(ch + "");
@@ -62,6 +88,8 @@ public class Lex {
             }
             pos++;
         }
+        // append the remaining values present
+        // in the StringBuilders
         appendTokens();
         return tokens.toArray(new String[0]);
     }
@@ -93,9 +121,15 @@ public class Lex {
         return !text.isBlank();
     }
 
+    // check if a character is in range
+    // of 0-9 or char is '.'
+
     public static boolean isDigit(char ch) {
         return '0' <= ch && ch <= '9' || ch == '.';
     }
+
+    // check if a characters is in range
+    // of a-zA-Z
 
     private static boolean isLetter(char ch) {
         return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z';
@@ -105,10 +139,15 @@ public class Lex {
         return ch == '(' || ch == ')';
     }
 
+    // append the builders to tokens
+    // and reset(clear) them
+
     private void appendTokens() {
         append(builder);
         append(number);
     }
+
+    // appends and clears the tokens
 
     private void append(StringBuilder builder) {
         final String build = builder.toString().trim();
